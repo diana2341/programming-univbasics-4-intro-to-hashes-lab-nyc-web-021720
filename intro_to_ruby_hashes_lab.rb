0cf30d87525a4e6c89987c67edc661d25458fd46
Diana Ponce <dianap2341@gmail.com>
2020-01-19 17:36:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-Hash[ key, value] → new_hash
+Hash[ key, value] => new_hash
 end
 # return a hash that includes the key and value parameters passed into this method
 
